83045bf88c08c05fb267ce03b0d080af1c96fb01
Jenny <jenny.m.lewis@hotmail.co.uk>
2021-12-31 15:09:45 +0000

    rock paper scissors game working, just not displaying user's choices

--- main.js
+++ main.js
@@ -18,24 +18,62 @@ function storeInput(){
 
 
 // On click of one the icons, randomly generate a computer move and select a winner
-    //Set icons as variables to use in an if function
-let userMoveRock = document.getElementById("rock");
-let userMovePaper = document.getElementById("paper");
-let userMoveScissors = document.getElementById("scissors");
+    //Set icons as variables to use in an if function with event listeners for a click
+let userMoveRock = document.getElementById("rock").addEventListener("click", userChoosesRock);
+let userMovePaper = document.getElementById("paper").addEventListener("click", userChoosesPaper);
+let userMoveScissors = document.getElementById("scissors").addEventListener("click", userChoosesScissors);
+    //functions for each icon to start the if statement below
+
 
     //Randomly generate a computer move and set it to a variable
         //Create an arraay containing rock, paper and scissors
-let moveChoices = ["rock", "paper", "scissors"];
+
         //math.random and math.floor to pick a random index between 0 and 2
-let computerMove = moveChoices[Math.floor(Math.random() * 3)];
-console.log(computerMove);
+
         //set value of random index to a variable
 
 
-
-
-
 // Display the winner and the winning choice
-
+    //if function to compare user's move with computer's move.
+        //if user wins, display their choice 1 entry
+        //if computer wins, display their choice 2 entry
+function userChoosesRock () {
+    let moveChoices = ["rock", "paper", "scissors"];
+    let computerMove = moveChoices[Math.floor(Math.random() * 3)];
+    console.log(computerMove);
+    if (computerMove === "rock") {
+        alert("Draw! Try again.");
+    } else if (computerMove === "scissors") {
+        alert("You win!", choiceOneInput);
+    } else {
+        alert("You lose!", choiceTwoInput);
+    }
+}
+        
+function userChoosesPaper () {
+    let moveChoices = ["rock", "paper", "scissors"];
+    let computerMove = moveChoices[Math.floor(Math.random() * 3)];
+    console.log(computerMove);
+    if (computerMove === "rock") {
+        alert("You win!", choiceOneInput);
+    } else if (computerMove === "scissors") {
+        alert("You lose!", choiceTwoInput);
+    } else {
+        alert("Draw! Try again.");
+    }      
+}
+        
+function userChoosesScissors () {
+    let moveChoices = ["rock", "paper", "scissors"];
+    let computerMove = moveChoices[Math.floor(Math.random() * 3)];
+    console.log(computerMove);
+    if (computerMove === "rock") {
+        alert("You lose!", choiceTwoInput);
+    } else if (computerMove === "scissors") {
+        alert("Draw! Try again.");
+    } else {
+        alert("You win!", choiceOneInput);
+    }  
+}
 
 
